5e3fb6a007b72786961af77fc96c8ca50ac0a23a
Johnny Winn <dev+nurugger07@gmail.com>
2012-07-31 16:25:12 -0400

    Add support for multiple markers

--- spec/cartographie/map_spec.rb
+++ spec/cartographie/map_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'cartographie/map'
+require 'uri'
 
 describe Cartographie::Map do
 
@@ -22,12 +23,12 @@ describe Cartographie::Map do
     its(:points) { should =~ (points) }
   end
 
-  describe "#additional_points" do
+  describe "#additional_markers" do
 
-    context "no additional points" do
+    context "no additional markers" do
       let(:map) { described_class.new('New York, NY') }
 
-      subject { map.additional_points }
+      subject { map.additional_markers }
 
       it 'returns an emtpy string' do
         subject.should be_empty
@@ -38,46 +39,84 @@ describe Cartographie::Map do
       let(:options) { { points: points } }
       let(:map) { described_class.new('New York, NY', options) }
 
-      subject { map.additional_points }
+      subject { map.additional_markers }
 
       it 'returns a pipe seperated string' do
-        subject.should eq("Empire State Building|40.704154,-73.99459|Guggenheim Museum")
+        subject.should eq("Empire+State+Building|40.704154,-73.99459|Guggenheim+Museum")
+      end
+    end
+  end
+
+  describe '#query_string' do
+
+    context 'build query string from hash of params' do
+      let(:options) { { width: 300, height: 300, zoom: 10, file_format: 'jpg', sensor: true } }
+      let(:map) { described_class.new('Tokyo', options) }
+
+      subject { map.query_string }
+
+      it 'returns a formatted query string' do
+        subject.should eq('center=Tokyo&size=300x300&zoom=10&format=jpg&sensor=true')
+      end
+    end
+
+    context 'build query string from params hash with additional points' do
+      let(:options) { { width: 300, height: 300, zoom: 10, file_format: 'jpg', sensor: true, points: ['Eiffel Tower', 'Louvre'] } }
+      let(:map) { described_class.new('Paris, FR', options) }
+
+      subject { map.query_string }
+
+      it 'returns a formatted query string with additional markers' do
+        subject.should eq('center=Paris,%20FR&size=300x300&zoom=10&format=jpg&sensor=true&markers=Eiffel+Tower%7CLouvre')
       end
     end
   end
 
   describe '#uri' do
-    let(:map) { described_class.new 'Tokyo' }
 
-    subject { map.uri }
+    context 'single marker uri' do
+      let(:map) { described_class.new 'Tokyo' }
 
-    it "should match the instance's string representation" do
-      subject.should eq(map.to_s)
+      subject { map.uri }
+
+      it "should match the instance's string representation" do
+        subject.should eq(map.to_s)
+      end
+
+      it 'returns a Google Static Maps URI' do
+        subject.should include('http://maps.googleapis.com/maps/api/staticmap')
+      end
+
+      it 'contains the map location' do
+        subject.should include(map.location)
+      end
+
+      it 'contains the map size, like 640x640' do
+        subject.should include(map.size)
+      end
+
+      it 'contains the map zoom level' do
+        subject.should include(map.zoom.to_s)
+      end
+
+      it 'contains the map file format' do
+        subject.should include(map.file_format)
+      end
+
+      it 'contains the map sensor indication' do
+        subject.should include(map.sensor.to_s)
+      end
     end
 
-    it 'returns a Google Static Maps URI' do
-      subject.should include('http://maps.googleapis.com/maps/api/staticmap')
-    end
+    context 'multiple markers' do
+      let(:options) { { points: points } }
+      let(:map) { described_class.new('New York, NY', options) }
 
-    it 'contains the map location' do
-      subject.should include(map.location)
-    end
+      subject { map.uri }
 
-    it 'contains the map size, like 640x640' do
-      subject.should include(map.size)
-    end
-
-    it 'contains the map zoom level' do
-      subject.should include(map.zoom.to_s)
-    end
-
-    it 'contains the map file format' do
-      subject.should include(map.file_format)
-    end
-
-    it 'contains the map sensor indication' do
-      subject.should include(map.sensor.to_s)
+      it 'contains the map additional markers' do
+        subject.should include(URI.escape(map.additional_markers))
+      end
     end
   end
-
 end
